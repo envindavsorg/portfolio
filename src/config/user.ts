3bdf71182adc32ddcce6a778708e0fb50211075c
Cuzeac Florin <mail@cuzeacflorin.fr>
2025-12-22 13:00:01 +0100

    update CV link

--- src/config/user.ts
+++ src/config/user.ts
@@ -90,7 +90,7 @@ Vous y trouverez un **aperçu complet** de mon expertise et de ma progression da
 Pour recevoir une **copie actualisée** directement dans votre boîte e-mail, cliquez sur le bouton ci-dessous.
 Je serai ravi d'échanger avec vous sur d'éventuelles opportunités de collaboration.
 `,
-			url: 'https://cfhi75vpdo.ufs.sh/f/tIhJKzZYPGQBmdsQlYPrfNCRIymQ6V25tWlH8nYXsxEKoUFL',
+			url: 'https://cfhi75vpdo.ufs.sh/f/tIhJKzZYPGQB90x1lnfzmMd4kitPhxzKc9jr1IvD6ZAR3VN5',
 			name: 'cv_florin_cuzeac.pdf',
 			title: 'Voir ou télécharger mon CV',
 		},
