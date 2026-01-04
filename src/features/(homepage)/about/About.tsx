4621b67d8e142b66d6d96a8a35fb2e6ff78054c2
Cuzeac Florin <mail@cuzeacflorin.fr>
2026-01-04 15:00:00 +0100

    refactor: update About component

--- src/features/(homepage)/about/About.tsx
+++ src/features/(homepage)/about/About.tsx
@@ -1,14 +1,24 @@
+'use client';
+
 import type React from 'react';
+import { useCallback, useState } from 'react';
 import { Panel } from '@/components/ui/Panel';
 import { AboutContent } from './AboutContent';
+import { AboutFooter } from './AboutFooter';
 import { AboutTitle } from './AboutTitle';
 
-const About = (): React.JSX.Element => (
-	<Panel id="about">
-		<AboutTitle />
-		<AboutContent />
-	</Panel>
-);
+const About = (): React.JSX.Element => {
+	const [isExpanded, setIsExpanded] = useState(false);
+	const toggleExpanded = useCallback(() => setIsExpanded((prev) => !prev), []);
+
+	return (
+		<Panel>
+			<AboutTitle />
+			<AboutContent expanded={isExpanded} />
+			<AboutFooter expanded={isExpanded} onToggle={toggleExpanded} />
+		</Panel>
+	);
+};
 
 About.displayName = 'About';
 
